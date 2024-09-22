c97216bbb6dc46100d5af71c661a95d7f2f4e56e
Zain Fathoni <me@zainf.dev>
2024-09-22 07:49:11 +0700

    about_keyword_argument

--- about_keyword_arguments.rb
+++ about_keyword_arguments.rb
@@ -7,10 +7,10 @@ class AboutKeywordArguments < Neo::Koan
   end
 
   def test_keyword_arguments
-    assert_equal __, method_with_keyword_arguments.class
-    assert_equal __, method_with_keyword_arguments
-    assert_equal __, method_with_keyword_arguments(one: 'one')
-    assert_equal __, method_with_keyword_arguments(two: 2)
+    assert_equal Array, method_with_keyword_arguments.class
+    assert_equal [1, 'two'], method_with_keyword_arguments
+    assert_equal ['one', 'two'], method_with_keyword_arguments(one: 'one')
+    assert_equal [1, 2], method_with_keyword_arguments(two: 2)
   end
 
   def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
@@ -18,10 +18,10 @@ class AboutKeywordArguments < Neo::Koan
   end
 
   def test_keyword_arguments_with_wrong_number_of_arguments
-    exception = assert_raise (___) do
+    exception = assert_raise (ArgumentError) do
       method_with_keyword_arguments_with_mandatory_argument
     end
-    assert_match(/__/, exception.message)
+    assert_match(/wrong number of arguments \(given 0, expected 1\)/, exception.message)
   end
 
   def method_with_mandatory_keyword_arguments(one:, two: 'two')
@@ -29,15 +29,15 @@ class AboutKeywordArguments < Neo::Koan
   end
 
   def test_mandatory_keyword_arguments
-    assert_equal __, method_with_mandatory_keyword_arguments(one: 'one')
-    assert_equal __, method_with_mandatory_keyword_arguments(two: 2, one: 1)
+    assert_equal ['one', 'two'], method_with_mandatory_keyword_arguments(one: 'one')
+    assert_equal [1, 2], method_with_mandatory_keyword_arguments(two: 2, one: 1)
   end
 
   def test_mandatory_keyword_arguments_without_mandatory_argument
-    exception = assert_raise(___) do
+    exception = assert_raise(ArgumentError) do
       method_with_mandatory_keyword_arguments
     end
-    assert_match(/__/, exception.message)
+    assert_match(/missing keyword: :one/, exception.message)
   end
 
 end
