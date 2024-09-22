5933c485b67b3c38a7248a1f83f4d23cd4dd4a40
Zain Fathoni <me@zainf.dev>
2024-09-22 11:45:36 +0700

    about_triangle_project_2

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,8 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError if a <= 0 || b <= 0 || c <= 0
+  raise TriangleError if a + b <= c || a + c <= b || b + c <= a
   return :equilateral if a == b && b == c
   return :isosceles if a == b || b == c || a == c
   return :scalene
